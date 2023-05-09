1cdf14b670d559958a64b455bc48838429f2614a
yungsabirka <75206908+yungsabirka@users.noreply.github.com>
2023-05-09 16:08:15 +0300

    Change ling on data site

--- RenderInfo.js
+++ RenderInfo.js
@@ -5,7 +5,7 @@ import {AddCityInfo} from "./CityInfo.js";
 export class RenderInfo{
 
     static setCityButton(cityId, selector, timeZone) {
-        let cityWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=bf35cac91880cb98375230fb443a116f`
+        let cityWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=bf35cac91880cb98375230fb443a116f`
         let cityForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=bf35cac91880cb98375230fb443a116f`;
         fetch(cityWeatherUrl)
             .then(response => response.json())
